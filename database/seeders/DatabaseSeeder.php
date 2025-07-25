94fd4dbd4c8e7a6ee4a902ac9af652eca83cba73
douma <doumaarmamd@gmail.com>
2025-07-25 12:38:16 +0000

    modif

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employee;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        function calculateMonthlyTotals(array $data, array $months): array {
+            $result = array_fill_keys($months, 0);
+            $grandTotal = 0;
+
+            foreach ($data as $item) {
+                foreach ($months as $month) {
+                    $value = isset($item[$month]) ? (float)$item[$month] : 0;
+                    $result[$month] += $value;
+                    $grandTotal += $value;
+                }
+            }
+
+            $result['total'] = $grandTotal;
+            return $result;
+        }
+
+        $employees = Employee::with('timeAllocations')
+        ->has('timeAllocations') // s'assure que l'employé a des timeAllocations
+        ->get();
+
+                    // Création des monthlyTotals
+            foreach ($employees as $employee) {
+                $data = $employee->timeAllocations->toArray(); // récupère les données des allocations
+                $totals = calculateMonthlyTotals($data, $months);
+                $totals['employeeId']= $employee->employeeId;
+                // Créer le monthly total
+                $employee->monthlyTotal()->create($totals);
+            }
     }
 }
